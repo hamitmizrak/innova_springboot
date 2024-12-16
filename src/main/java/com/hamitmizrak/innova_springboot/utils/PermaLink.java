335cf93c083ab9ef3bb098e11edf71964e3f4d83
Hamit Mizrak <hamitmizrak@gmail.com>
2024-12-16 14:06:36 +0300

    PermaLink

---
+++ src/main/java/com/hamitmizrak/innova_springboot/utils/PermaLink.java
@@ -0,0 +1,10 @@
+package com.hamitmizrak.innova_springboot.utils;
+
+public class PermaLink {
+    /*
+    http://localhost:4444
+    http://localhost:4444/h2-console
+    http://localhost:4444/swagger-ui/index.html
+    http://localhost:4444/actuator/healths
+    */
+}
