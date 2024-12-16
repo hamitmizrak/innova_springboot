927cc42c657aac5ea93799c42be866405d6af51a
Hamit Mizrak <hamitmizrak@gmail.com>
2024-12-16 15:05:40 +0300

    Exception

---
+++ src/main/java/com/hamitmizrak/innova_springboot/error/ApiResult.java
@@ -0,0 +1,4 @@
+package com.hamitmizrak.innova_springboot.error;
+
+public class ApiResult {
+}
---
+++ src/main/java/com/hamitmizrak/innova_springboot/exception/_404_NotFoundException.java
@@ -0,0 +1,16 @@
+package com.hamitmizrak.innova_springboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// Status Code           : 500
+// INTERNAL_SERVER_ERROR : Server Hatası
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class _404_NotFoundException extends RuntimeException {
+
+    // Parametreli Constructor
+    public _404_NotFoundException(String message) {
+        super(message);
+    } //end parametreli constructor
+
+} //end class
