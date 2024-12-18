94d51e9d9f3f1323636b60e5f98a9317db4be8d1
Hamit Mizrak <hamitmizrak@gmail.com>
2024-12-18 15:32:59 +0300

    Sequence, @Table, @Index, uniqueConstraints, catalog, schema

--- src/main/java/com/hamitmizrak/innova_springboot/data/entity/AddressEntity.java
+++ src/main/java/com/hamitmizrak/innova_springboot/data/entity/AddressEntity.java
@@ -13,9 +13,31 @@ import lombok.*;
 //@NoArgsConstructor
 //@Builder
 
+/*
+name = "addreseses" => Databesdeki tablo adı
+schema = "public"   => Postgresql vb yapılarında şema yapısını destekleten veri tablarında tabloya erişim için kullanılır.
+SELECT * FROM public.addreseses
+catalog = "blog"    => Mysql vb gibi veritabanlarında
+uniqueConstraints = @UniqueConstraint(columnNames = {"state"}) => benzersiz sutun verisi için
+
+indexes = {
+                @Index(name="idx_address_city",columnList = "city",unique = true)
+        }
+        columnList = "city": Indeksin uygulanacağı sütün adıdır. birden fazla sutun eklemek için virgül yazılır
+        unique = true: Indeksin benzersiz olmasını sağlar
+*/
+
 // Entity
 @Entity(name = "Addreses")
-@Table(name = "addreseses")
+@Table(
+        name = "addreseses",
+        schema = "public",
+        catalog = "blog",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"state"}),
+        indexes = {
+                @Index(name="idx_address_city",columnList = "city",unique = true)
+        }
+)
 // Address
 public class AddressEntity extends BaseEntity{
 
@@ -25,6 +47,15 @@ public class AddressEntity extends BaseEntity{
     // Dikkat: Sorting için burada yazan: addresseEntityDetailsEmbedable göre işlem yapılır
     @Embedded
     private AddresseEntityDetailsEmbedable addresseEntityDetailsEmbedable;
+
+    // Database tablo oalrak eklenmesin: sadece Java olsun ancak database sutun adı olması
+    @Transient
+    private String temporaryData;
+
+    // Optimistik Kilitlenme (Optimistic Locking)
+    // Entity'timizin versiyonlararak saklanması
+    @Version
+    private int version;
 
     // RELATION
 }
--- src/main/java/com/hamitmizrak/innova_springboot/data/entity/BaseEntity.java
+++ src/main/java/com/hamitmizrak/innova_springboot/data/entity/BaseEntity.java
@@ -1,6 +1,5 @@
 package com.hamitmizrak.innova_springboot.data.entity;
 
-import com.hamitmizrak.innova_springboot.audit.AuditingAwareBaseDto;
 import com.hamitmizrak.innova_springboot.audit.AuditingAwareBaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -21,10 +20,34 @@ abstract public class BaseEntity extends AuditingAwareBaseEntity implements Seri
     public static final Long serialVersionUID = 456546546546456412L;
 
     // FIELD
-    // ID
+    /*
+    GeneratedValue => Birincil Anahtar(PK) nasıl üretileceğini belirtmek için kullanırız.
+    GeneratedValue:
+    AUTO: Varsayılan Stratejidir. Örnek: Postgresql
+    IDENTITY: Veritabanlarında otomatik artırım (AI) içindir.  Örnek: Mysql, H2DB
+    SEQUNCE: Sıra kullanmak. Örnek: Oracle, Mysql, Postgresql
+    TABLE: Tablo kullanmak içindir
+     */
+
+    // 1.YOL
+    // Neden Sequence kullanıyoruz ?
+    /*
+    Performans: Diziler diğer yöntemler olan IDENTITY, AUTO vb yapılara göre daha performansı
+    Taşınabilirlik: JPA'in birden fazla Mysql, Postgresql, Oracle, MariaDB, H2DB vb
+    Kontrol: allocationSize başlangıç değer artışını kontrol edebilmek
+    */
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "address_seq")
+    @SequenceGenerator(name="address_seq",sequenceName = "address_sequence",allocationSize=1)
     protected Long id;
+
+    // 2.YOL
+    /*
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Mysql
+    @GeneratedValue(strategy = GenerationType.AUTO) //Postgresql
+    protected Long id;
+    */
 
     // SYSTEM DATE
     @CreationTimestamp
